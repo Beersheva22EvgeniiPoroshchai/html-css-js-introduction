b956338f72322b0d4907e844b644931651d922d2
Joebye <evgenii.prs@gmail.com>
2023-05-07 22:21:52 +0300

    Cargram solution with min animation

--- html-css-js-introduction/src/cars.js
+++ html-css-js-introduction/src/cars.js
@@ -1,16 +1,44 @@
 const detailsImageElement = document.querySelector(".details-image");
 const detailsTitleElement = document.querySelector(".details-title");
 const thumbnailsAnchors = document.querySelectorAll(".thumbnails-ancor");
+const detailsSectionElement = document.querySelector(".details-section");
+
+const mainSection = document.querySelector("main") 
+
+const HIDDEN = "hidden";
+const POINT_CLASS = "is-point";
+
 for (let i = 0; i < thumbnailsAnchors.length; i++) {
     thumbnailsAnchors[i].addEventListener("click", function() {
         setDetails(i); 
 
 })
 }
+
 function setDetails(indexAnchor) {
+    showDetails();
+
     detailsImageElement.src = thumbnailsAnchors[indexAnchor].getAttribute("data-details-image");
     detailsTitleElement.innerHTML = thumbnailsAnchors[indexAnchor].getAttribute("data-details-text");
 
+}
+
+
+function showDetails() {
+    mainSection.classList.remove(HIDDEN);
+    detailsSectionElement.classList.add(POINT_CLASS);
+    
+    setTimeout(function() {
+        detailsSectionElement.classList.remove(POINT_CLASS);
+
+
+    })
+
+
+}
+
+function hideDetails() {
+    mainSection.classList.add(HIDDEN);
 }
 
 
