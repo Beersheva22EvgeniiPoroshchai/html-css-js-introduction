a08492187058daa7bef809b1f500b2053c1017e5
Joebye <evgenii.prs@gmail.com>
2023-05-08 21:22:06 +0300

    upd Cargram

---
+++ html-css-js-introduction/app.js
@@ -0,0 +1,115 @@
+// let a = 5;
+// a = "hello";
+// a = false;
+// let a = 5 + "5";
+// console.log(a);
+// console.log(a, typeof(a));
+// a = a - 5;
+// console.log(a, typeof(a));
+// a = "abc";
+// a /= 2;
+// console.log(a, typeof(a));
+// a += "def";
+// console.log(a);
+// a = 12;
+// a *= 1.5;
+// console.log(a);
+// a = "123";
+// a=+a;
+// console.log(a, typeof(a));
+// if (a = 1) {
+//     console.log("a is true");
+// }
+// console.log(a);
+
+// let a = 1;
+// let b = 3;
+// let c = "2";
+// if (a < b <= c) {
+//     console.log("a<b<c");
+// }
+// console.log(a,b,c);
+
+
+// if (2 > "abc") {
+//     console.log("true")
+// }
+
+// console.log("2", "abc");
+
+// let a = "abc";
+// a= !!a;
+
+
+// console.log(a,typeof(a));
+
+// console.log(2+10*false/true);
+
+// console.log(10*false/true);
+
+// console.log(false/true);
+
+// let a = new Number(3);
+// console.log(a, typeof(a));
+
+
+// let b = 0.345678;
+// console.log(b, typeof(b));
+// b = b.toFixed(4);
+// console.log(b, typeof(b));
+
+// a = !!new Number(1);
+// console.log(a, typeof a);
+
+// a = !!0;
+// console.log(a, typeof a);
+
+// f = new String("2");
+
+// g = 5;
+// l = f -g;
+// console.log(l, typeof l);
+
+// console.log(false/true);
+
+// a = 0.6;
+// Math.trunc(a);
+
+// console.log(a);
+
+
+
+// let a = "256 * 320";
+// console.log(a);
+// console.log(parseInt(a));
+
+// a = 257.45;
+// console.log(parseFloat(a));
+// a = "257.4a";
+// console.log(+a, parseInt(a), parseFloat(a));
+
+// String.length(console.log("abc"));
+
+// a = "";
+// console.log (a.length);
+// a = 123;
+// console.log(a.toString(16));
+
+
+
+console.log(("A" + + "A" + "AS").toLowerCase());
+
+function myToStringInt(number, radix) {
+    
+    
+    //number - any number
+    //radix - computation base if radix < 2 or radix > 36 -> radix = 10;
+    //removes fraction part, 34.25 -> 34; if 34.75 -> 35;
+    //returns string as a view presentation of the intger part of a given number 
+    // 
+
+}
+
+console.log((123456789).toString(36));
+
+
